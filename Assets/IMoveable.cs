8e79bf3805820bb7676960f8970530b22b33c2f6
Julia <juliasp@kth.se>
2017-10-17 12:05:57 +0200

    new stuff
    updated unity version + buttons and lever

---
+++ Assets/IMoveable.cs
@@ -0,0 +1,4 @@
+﻿internal interface IMoveable
+{
+    void UpdatePosition();
+}
---
+++ Assets/moveCube.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCube : MonoBehaviour, IMoveable {
+	
+	public void UpdatePosition () {
+        Debug.Log("MOVING");
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
